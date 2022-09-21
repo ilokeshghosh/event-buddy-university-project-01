f674b8f73d4d20956e10b93d8a1b82c180c3fbf5
Lokesh Ghosh <lghosh857@gmail.com>
2022-09-22 01:27:33 +0530

    updated login.php

--- login.php
+++ login.php
@@ -1,30 +1,29 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "test_db";
+session_start();
+ include "db_conn.php";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
+    // if($_SERVER["REQUEST_METHOD"]=="POST"){ 
+    if (isset($_POST['event_id']) && isset($_POST['p_password']) && isset($_POST['p_email'])) {
+    
+        $event_id=$_POST['event_id'];
+        $p_email=$_POST['p_email'];
+        $p_password=$_POST['p_password'];
 
-// sql to create table
-$sql = "CREATE TABLE MyGuests (
-id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-firstname VARCHAR(30) NOT NULL,
-lastname VARCHAR(30) NOT NULL,
-email VARCHAR(50),
-reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-)";
+        if(empty($event_id && $p_email && $p_password)){
+            echo "Enter all data";
+        }
+        else{
+            echo"$event_id";
+            echo"$p_email";
+            echo"$p_password";
+        }
 
-if ($conn->query($sql) === TRUE) {
-  echo "Table MyGuests created successfully";
-} else {
-  echo "Error creating table: " . $conn->error;
-}
+       
+    }
 
-$conn->close();
+
+ 
+
+
+
 ?>
