05e3ebd7b9d3b563b691129cd837ca6697c9786b
Lokesh Ghosh <lghosh857@gmail.com>
2022-09-22 01:46:03 +0530

    Update login.php

--- login.php
+++ login.php
@@ -5,17 +5,47 @@ session_start();
     // if($_SERVER["REQUEST_METHOD"]=="POST"){ 
     if (isset($_POST['event_id']) && isset($_POST['p_password']) && isset($_POST['p_email'])) {
     
-        $event_id=$_POST['event_id'];
-        $p_email=$_POST['p_email'];
-        $p_password=$_POST['p_password'];
+        
+        function validate($data){
+            $data =trim($data);
+            $data=stripslashes($data);
+            $data=htmlspecialchars($data);
+            return $data;
+        }
 
-        if(empty($event_id && $p_email && $p_password)){
-            echo "Enter all data";
+        $event_id=validate($_POST['event_id']);
+        $p_email=validate($_POST['p_email']);
+        $p_password=validate($_POST['p_password']);
+
+        if(empty($event_id)){
+            header("Location: signup-login.html?error=Event Id is required");
+            exit();
+        }
+        elseif(empty($p_email)){
+            header("Location: signup-login.html?error=Mail Id is required");
+            exit();
+
+        }
+        elseif(empty($p_password)){
+            header("Location: signup-login.html?error=Password is required");
+            exit();
+
         }
         else{
-            echo"$event_id";
-            echo"$p_email";
-            echo"$p_password";
+            $sql= "SELECT * FROM participants WHERE event_id='$event_id' AND p_password='$p_password' AND p_email='$p_email'";
+            $result=$conn->query($sql);
+            // echo "$result";
+            if ($result->num_rows > 0) {
+                // output data of each row
+                while($row = $result->fetch_assoc()) { 
+                  // the function fetch_assoc() puts all the results into an associative array($row) 
+              
+                  echo "Event_id: " . $row["event_id"]. " - p_email: " . $row["p_email"]. " " . $row["p_password"]. "<br>";
+                }
+              } else {
+                echo "0 results";
+              }
+
         }
 
        
